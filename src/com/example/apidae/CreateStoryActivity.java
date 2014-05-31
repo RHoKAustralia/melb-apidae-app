6089ae6cf7b0163a7861579599a2865196d92a62
konrad-c <ironeye42@gmail.com>
2014-05-31 20:50:41 +1000

    update

--- src/com/example/apidae/CreateStoryActivity.java
+++ src/com/example/apidae/CreateStoryActivity.java
@@ -19,7 +19,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class CreateStoryActivity extends Activity{
     private static final int CAMERA_REQUEST = 1888;
@@ -55,45 +57,86 @@ public class CreateStoryActivity extends Activity{
 	
 	public void onAddPhotoClick(View v){
 		
-		final CharSequence[] items = { "Take Photo", "Choose from Library",
+		final CharSequence[] items = { "Take Photo",/* "Choose from Library",*/
 		"Cancel" };
 		
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Add Photo!");
+		builder.setTitle("Add Photo");
 		builder.setItems(items, new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int item) {
 				if (items[item].equals("Take Photo")) {
 					Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
 	                startActivityForResult(cameraIntent, CAMERA_REQUEST); 
-				} else if (items[item].equals("Choose from Library")) {
+				} /*else if (items[item].equals("Choose from Library")) {
 					Intent i = new Intent(
 							Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 							 
 							startActivityForResult(i, SELECT_FILE);
-				} else if (items[item].equals("Cancel")) {
+				}*/ else if (items[item].equals("Cancel")) {
 					dialog.dismiss();
 				}
 			}
 		});
 		builder.show();
-		//Intent photoInt = new Intent(v.getContext(), TakePhotoActivity.class);
-		//startActivity(photoInt);
+	}
+	
+	private void updateImageScroll(){
+		imageScrollLayout.removeAllViews();
+		for( Bitmap b : photos){
+			ImageButton picB = new ImageButton(this);
+			picB.setImageBitmap(b);
+			
+			LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			imageScrollLayout.addView(picB, lp);
+		}
+		Button picB = new Button(this);
+		picB.setText("Add Photo");
+		picB.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				final CharSequence[] items = { "Take Photo",/* "Choose from Library",*/
+				"Cancel" };
+				
+				AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+				builder.setTitle("Add Photo");
+				builder.setItems(items, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int item) {
+						if (items[item].equals("Take Photo")) {
+							Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
+			                startActivityForResult(cameraIntent, CAMERA_REQUEST); 
+						} /*else if (items[item].equals("Choose from Library")) {
+							Intent i = new Intent(
+									Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+									 
+									startActivityForResult(i, SELECT_FILE);
+						}*/ else if (items[item].equals("Cancel")) {
+							dialog.dismiss();
+						}
+					}
+				});
+				builder.show();
+			}
+		});
+		
+		LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		imageScrollLayout.addView(picB, lp);
 	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
 		super.onActivityResult(requestCode, resultCode, data); 
-		Bitmap b;
 		switch(requestCode) {
 			case CAMERA_REQUEST:  
 			    if(resultCode == RESULT_OK){ 
 		            Bitmap photo = (Bitmap) data.getExtras().get("data"); 
-		            b = photo;
+		            photos.add(photo);
 			    }
             	break;
-            	
+            /*	
 			case SELECT_FILE:
-			    if(resultCode == SELECT_FILE){  
+			    if(resultCode == RESULT_OK){  
 			    	Uri selectedImage = data.getData();
 			         String[] filePathColumn = { MediaStore.Images.Media.DATA };
 			 
@@ -105,9 +148,10 @@ public class CreateStoryActivity extends Activity{
 			         String picturePath = cursor.getString(columnIndex);
 			         cursor.close();
 			         
-			         b = BitmapFactory.decodeFile(picturePath);
+			         photos.add(BitmapFactory.decodeFile(picturePath));
 			    }
-			    break;
+			    break;*/
 		}
+		updateImageScroll();
     } 
 }
