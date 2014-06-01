9ae586f0a60f1d567b5d2c6b40c609db96cf4b6c
ipla2 <ipla2@JMSSX220PLA0001.AD.MONASH.EDU>
2014-06-01 11:39:05 +1000

    Map layout/activity updated for google maps

--- src/com/example/apidae/MapActivity.java
+++ src/com/example/apidae/MapActivity.java
@@ -2,14 +2,33 @@ package com.example.apidae;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class MapActivity extends Activity {
 	
 	private GoogleMap map;
 	
 	public void onCreate(Bundle savedInstanceState) {
-	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map_layout);
+		
+		try {
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	private void initializeMap() {
+		if (map == null) {
+			map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			
+			if (map == null) {
+				Toast.makeText(getApplicationContext(), "Unable to create map", Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 }
